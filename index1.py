b4d147f7a6fff1f29b46b70a43f70a44f7e875d3
Natalia <nat.dol30@mail.ru>
2019-03-01 23:28:52 +0300

    -- Bootstrap – знакомство

--- index1.py
+++ index1.py
@@ -46,41 +46,68 @@ def sample():
                        <title>Bootstrap – знакомство</title>
                      </head>
                      <body>
-                       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                          <a class="navbar-brand" href="#">Это</a>
-                          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                       <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+                          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                           </button>
-                          <div class="collapse navbar-collapse" id="navbarNav">
+                          <a class="navbar-brand" href="#">Navbar</a>
+                          <div class="collapse navbar-collapse" id="navbarNavDropdown">
                             <ul class="navbar-nav">
                               <li class="nav-item active">
-                                <a class="nav-link" href="#">задача<span class="sr-only">(current)</span></a>
+                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                               </li>
                               <li class="nav-item">
-                                <a class="nav-link" href="#">из</a>
+                                <a class="nav-link" href="#">Features</a>
                               </li>
                               <li class="nav-item">
-                                <a class="nav-link" href="#">яндекс</a>
+                                <a class="nav-link" href="#">Pricing</a>
                               </li>
-                              <li class="nav-item">
-                                <a class="nav-link disabled" href="#">лицея</a>
+                              <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                  Dropdown link
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                  <a class="dropdown-item" href="#">Action</a>
+                                  <a class="dropdown-item" href="#">Another action</a>
+                                  <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
                               </li>
                             </ul>
                           </div>
-                          <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-                              <div class="carousel-inner">
-                                <div class="carousel-item active">
-                                  <img class="d-block w-100" src="{}" alt="First slide">
-                                </div>
-                                <div class="carousel-item">
-                                  <img class="d-block w-100" src="{}" alt="Second slide">
-                                </div>
-                                <div class="carousel-item">
-                                  <img class="d-block w-100" src="{}" alt="Third slide">
-                                </div>
-                              </div>
-                            </div>
                         </nav>
+                        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                          <!-- Indicators -->
+                          <ol class="carousel-indicators">
+                            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                            <li data-target="#myCarousel" data-slide-to="1"></li>
+                            <li data-target="#myCarousel" data-slide-to="2"></li>
+                          </ol>
+                        
+                          <!-- Wrapper for slides -->
+                          <div class="carousel-inner">
+                            <div class="item active">
+                              <img src="{}">
+                            </div>
+                        
+                            <div class="item">
+                              <img src="{}">
+                            </div>
+                        
+                            <div class="item">
+                              <img src="{}">
+                            </div>
+                          </div>
+                        
+                          <!-- Left and right controls -->
+                          <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left"></span>
+                            <span class="sr-only">Previous</span>
+                          </a>
+                          <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right"></span>
+                            <span class="sr-only">Next</span>
+                          </a>
+                        </div>
                     </html>'''.format(url_for('static', filename='img/bird.jpeg'),
                                       url_for('static', filename='img/fox.jpg'), url_for('static',
                                                                                          filename='https://sweetpanda.ru/wp-content/uploads/2016/10/gettyimages-73726710.jpg'))
