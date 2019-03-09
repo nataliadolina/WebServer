0d43c14e8a4f273b9fdfc9bfd913198b50355990
Natalia <nat.dol30@mail.ru>
2019-03-09 16:28:23 +0300

    -- Новости. Наследование

---
+++ новости.наследование.py
@@ -0,0 +1,16 @@
+import json
+
+from flask import Flask, request, url_for, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/news', methods=['POST', 'GET'])
+def news():
+    with open("static/json_files/news", "rt", encoding="utf8") as f:
+        news_list = json.loads(f.read())
+    return render_template('news.html', news=news_list)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
